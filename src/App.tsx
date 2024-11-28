c6432aa6a5fd97e18b299c427744de829b70e80d
wen-2018 <42974891+wen-2018@users.noreply.github.com>
2024-11-27 21:34:11 -0400

    dynamic distance

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,55 @@
-import './App.css'
+import "./App.css";
 
 function App() {
+  function handleOnDistanceChange(event: { currentTarget: { value: string } }) {
+    hideAnimation();
+    const value = Number(event.currentTarget.value);
+    const percent = ((value / 2863) * 100).toFixed(2);
+    document.documentElement.style.setProperty(
+      "--complete-percentage",
+      `${percent}%`
+    );
+    document.documentElement.style.setProperty(
+      "--animation-duration",
+      `${value * 0.003}s`
+    );
+  }
+
+  function showAnimation() {
+    const sliceContainer =
+      document.documentElement.getElementsByClassName("slice-container")[0];
+    sliceContainer.classList.remove("hide-animation");
+    sliceContainer.classList.add("show-animation");
+    console.log("clicked");
+  }
+
+  function hideAnimation() {
+    const sliceContainer =
+      document.documentElement.getElementsByClassName("slice-container")[0];
+    sliceContainer.classList.add("hide-animation");
+    sliceContainer.classList.remove("show-animation");
+  }
 
   return (
-    <>
+    <div>
+      <label htmlFor="distance">Current distance (km):</label>
+      <input
+        type="text"
+        id="name"
+        name="distance"
+        onChange={handleOnDistanceChange}
+      />
+      <button onClick={() => showAnimation()}>Show Animation</button>
       <div className="main-container">
-        <div className="slice-container">
+        <div className="slice-container hide-animation">
           <img className="slice" src="slice.svg" alt="" />
         </div>
         <div className="image-container">
           <img src="map-with-path.jpg" alt="" />
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
-export default App
+export default App;
